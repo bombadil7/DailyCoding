e0b59a1ec30055a5a90cab46ce5833bf3b08051d
Andrei Kniazev <akniazev@gmail.com>
2018-11-29 01:26:14 +0000

    Implement serialize

--- prob3/tree.py
+++ prob3/tree.py
@@ -6,7 +6,22 @@ class Node:
 
 
 def serialize(node):
-    node
+    serial = node.val 
+
+    if node.left or node.right:
+        serial += r'('
+
+        if node.left:
+            serial += serialize(node.left)
+            
+        serial += r'|' 
+            
+        if node.right:
+            serial += serialize(node.right)
+            
+        serial += r')'
+
+    return serial
 
 
 def deserialize(string):
@@ -19,9 +34,11 @@ def deserialize(string):
 
 
 def main():
-    #node = Node('root', Node('left', Node('left.left')), Node('right'))
+    node = Node('root', Node('left', Node('left.left')), Node('right'))
+    #node = Node('root', Node('left', Node('left.left')))
     #assert deserialize(serialize(node)).left.left == 'left.left'
-    deserialize("left{root}right")
+    #deserialize("left{root}right")
+    print(serialize(node))
 
 if __name__ == "__main__":
     main()
