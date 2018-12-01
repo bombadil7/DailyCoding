fadc9b8356f9cc69dac6866b97cddb5c4acf8da1
Andrei Kniazev <akniazev@gmail.com>
2018-12-01 16:19:52 +0000

    Clean up product python

--- prob2/product_array.py
+++ prob2/product_array.py
@@ -2,14 +2,21 @@ from functools import reduce
 # The goal is to produce a new array, in which every
 # item is the product of all other elements of the 
 # original array
-array = [2, 4, 5, 2, 3]
+
 def get_remaining(lst, ind):
     return [x for i, x in enumerate(lst) if i != ind]
 
-all_subarrays = list(get_remaining(array, i) for i in range(len(array)))
-new_array = []
-for sub in all_subarrays:
-    new_array.append(reduce((lambda x,y: x*y), sub))
-#new_array = list(reduce((lambda x,y: x*y), subarray for subarray in all_subarrays))
-print(new_array)
-#new_array = list(map(reduce((lambda x, y: x*y), sub_array for sub_array in ), sub_array))
+
+def main():
+        array = [2, 4, 5, 2, 3]
+        all_subarrays = list(get_remaining(array, i) for i in range(len(array)))
+        new_array = []
+        for sub in all_subarrays:
+                new_array.append(reduce((lambda x,y: x*y), sub))
+        #new_array = list(reduce((lambda x,y: x*y), subarray for subarray in all_subarrays))
+        print(new_array)
+        #new_array = list(map(reduce((lambda x, y: x*y), sub_array for sub_array in ), sub_array))
+
+
+if __name__ == "__main__":
+        main()
