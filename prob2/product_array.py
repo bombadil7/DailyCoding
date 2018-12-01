8db4c0a4c79b669ba60ea964e2889d7d5e61a291
Andrei Kniazev <akniazev@gmail.com>
2018-12-01 21:20:47 +0000

    Replace functions with lambdas for compactness

--- prob2/product_array.py
+++ prob2/product_array.py
@@ -3,18 +3,19 @@ from functools import reduce
 # item is the product of all the elements of the 
 # original array except current element
 
-def remove_current_element(lst, index):
-    """ Given a list and index of one element we remove that element from the list """
-    return [x for i, x in enumerate(lst) if i != index]
-
-def multiply_elements(lst):
-    """ Given a list we multiply all the element of it and return as a single number """
-    return reduce((lambda x,y: x*y), lst)
-
 def main():
     array = [2, 4, 5, 2, 3]
 
+    # Given a list and index of one element we remove that element from the list
+    remove_current_element = lambda lst, index: [x for i, x in enumerate(lst) if i != index]
+
+    # Given a list we multiply all elements of it and return as a single number
+    multiply_elements = lambda lst: reduce((lambda x,y: x*y), lst)
+
+    # Create a list of new lists with required elements removed
     all_subarrays = list(remove_current_element(array, i) for i in range(len(array)))
+
+    # Reduce each sub-list to a single number, which is a product of all elements
     new_array = list(map(multiply_elements,[x for x in all_subarrays]))
 
     print(new_array)
